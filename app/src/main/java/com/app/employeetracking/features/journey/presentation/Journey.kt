33de8b01eecabb1cceb9dde3fec38e2d790870da
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2024-06-10 09:13:27 +0545

    add journey tabs

--- app/src/main/java/com/app/employeetracking/features/journey/presentation/Journey.kt
+++ app/src/main/java/com/app/employeetracking/features/journey/presentation/Journey.kt
@@ -1,10 +1,26 @@
 package com.app.employeetracking.features.journey.presentation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
 fun Journey(modifier: Modifier = Modifier) {
-    Text("joureny")
+    var currentIndex by remember { mutableIntStateOf(0) }
+    val tabs = listOf("Journey", "History", "Holiday", "Requests")
+    Column(modifier) {
+        TabRow(modifier = Modifier.fillMaxWidth(),selectedTabIndex = currentIndex) {
+            tabs.forEachIndexed { index, title ->
+                Tab(text = { Text(title) }, selected = index == currentIndex, onClick = {currentIndex = index})
+            }
+        }
+    }
 }
