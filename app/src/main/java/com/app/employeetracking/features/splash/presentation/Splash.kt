79ba4de75f3beb2f09b33146bc91274459b0b782
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2024-06-16 09:19:01 +0545

    add launched effect to goto login

--- app/src/main/java/com/app/employeetracking/features/splash/presentation/Splash.kt
+++ app/src/main/java/com/app/employeetracking/features/splash/presentation/Splash.kt
@@ -2,15 +2,21 @@ package com.app.employeetracking.features.splash.presentation
 
 import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import com.app.employeetracking.R
+import kotlinx.coroutines.delay
 
 @Composable
-fun Splash(modifier: Modifier = Modifier) {
+fun Splash(modifier: Modifier = Modifier, gotoLogin: () -> Unit = {}) {
     Image(
         painter = painterResource(R.drawable.splash),
         contentDescription = "splash screen for tracking app"
     )
+    LaunchedEffect(Unit) {
+        delay(3000L)
+        gotoLogin()
+    }
 }
 
