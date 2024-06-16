aa501b8e841bc286ce3298c97bf68a388870dbdb
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2024-06-16 16:10:27 +0545

    refactor route structure

---
+++ app/src/main/java/com/app/employeetracking/features/JobTrackerApp.kt
@@ -0,0 +1,126 @@
+package com.app.employeetracking.features
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import kotlinx.serialization.Serializable
+
+//app routes
+@Serializable
+object Splash
+
+@Serializable
+object Dashboard
+
+@Serializable
+object Home
+
+@Serializable
+object Jobs
+
+@Serializable
+object Journey
+
+@Serializable
+object Trunk
+
+@Serializable
+object Profile
+
+//auth routes
+@Serializable
+object Auth
+
+@Serializable
+object Login
+
+@Serializable
+object Forgot
+
+
+@Composable
+fun JobTrackerApp() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = {
+            DashboardBottomBar()
+        }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Journey,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable<Journey> { Text("journey") }
+            composable<Jobs> { Text("Jobs") }
+            composable<Home> { Text("Home") }
+            composable<Trunk> { Text("Trunk") }
+            composable<Profile> { Text("Profile") }
+        }
+    }
+}
+
+data class DashboardItem(
+    val label: String,
+    val icon: ImageVector
+)
+
+@Composable
+fun DashboardBottomBar(modifier: Modifier = Modifier) {
+    var selectedIndex by remember { mutableIntStateOf(0) }
+    val items = listOf(
+        DashboardItem(
+            label = "Journey",
+            icon = Icons.Default.PlayArrow
+        ),
+        DashboardItem(
+            label = "Jobs",
+            icon = Icons.Default.Build
+        ),
+        DashboardItem(
+            label = "Home",
+            icon = Icons.Default.Home
+        ),
+        DashboardItem(
+            label = "Trunk",
+            icon = Icons.Default.Menu
+        ),
+        DashboardItem(
+            label = "Profile",
+            icon = Icons.Default.Person
+        ),
+    )
+    NavigationBar {
+        items.forEachIndexed { index, dashboardItem ->
+            NavigationBarItem(
+                selected = selectedIndex == index,
+                onClick = {},
+                icon = {
+                    Icon(
+                        imageVector = items[index].icon,
+                        contentDescription = items[index].label
+                    )
+                }
+            )
+        }
+    }
+}
--- app/src/main/java/com/app/employeetracking/features/MainActivity.kt
+++ app/src/main/java/com/app/employeetracking/features/MainActivity.kt
@@ -4,16 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.app.employeetracking.core.routes.JobTrackerGraph
 import com.app.employeetracking.core.theme.EmployeeTrackingTheme
-import com.app.employeetracking.features.splash.presentation.Splash
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +12,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EmployeeTrackingTheme {
-                JobTrackerGraph()
+                JobTrackerApp()
             }
         }
     }
