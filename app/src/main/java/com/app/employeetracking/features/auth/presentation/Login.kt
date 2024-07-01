66c4fead30eb9026cb5ac1b095f65c59d2402b07
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2024-07-01 18:58:09 +0545

    add login page graphic

--- app/src/main/java/com/app/employeetracking/features/auth/presentation/Login.kt
+++ app/src/main/java/com/app/employeetracking/features/auth/presentation/Login.kt
@@ -1,10 +1,35 @@
 package com.app.employeetracking.features.auth.presentation
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.app.employeetracking.R
 
 @Composable
 fun Login(modifier: Modifier = Modifier) {
-    Text("login page")
+    Surface(modifier.fillMaxSize(), color = MaterialTheme.colorScheme.outlineVariant) {
+        Box {
+            Image(
+                painter = painterResource(R.drawable.logo),
+                contentDescription = null,
+                modifier = Modifier.align(
+                    Alignment.TopCenter
+                )
+            )
+            Image(
+                painter = painterResource(R.drawable.login_graphic),
+                contentDescription = null,
+                modifier = Modifier.align(
+                    Alignment.Center
+                )
+            )
+        }
+    }
 }
